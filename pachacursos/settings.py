ef34bd7ffcc2990498f78a3b970a95bd4e30577b
Dieguidev <diegogaraycullas@gmail.com>
2023-07-06 22:54:26 -0500

    creando archivo azure_storage

--- pachacursos/settings.py
+++ pachacursos/settings.py
@@ -133,10 +133,42 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+DEFAULT_FILE_STORAGE = 'hackatonFinal.azure_storage.AzureMediaStorage'
+
+STATICFILES_STORAGE = 'hackatonFinal.azure_storage.AzureStaticStorage'
+
+
+
+
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
+
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+
+
+
+
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/staticgpr3/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+
+
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/mediagpr3/'
+
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+
+
 
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
